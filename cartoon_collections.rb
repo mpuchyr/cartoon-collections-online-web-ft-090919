df617b95daaee13eb5cfa548afcfe17e671ab94f
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-10 22:01:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if foods.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
 end

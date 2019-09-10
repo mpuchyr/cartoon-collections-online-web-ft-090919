c11b6f9b520aa343b1f5e01f6530983f9a573a28
Matt Puchyr <matt.puchyr@gmail.com>
2019-09-10 21:54:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(elements)
-  summoning = []
-  
+  elements.map do |element|
+    element = element.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
